91a974ebe9468dbae7b59164b9ea507478656cd2
Muhammad Aryasatya Triputra <m.aryasatya.triputra@gmail.com>
2025-09-14 09:36:49 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -1,47 +1,48 @@
 package com.example.lab_week_03
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+interface CoffeeListener {
+    fun onSelected(id: Int)
+}
+
+class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_fragment)) { v,
-                                                                             insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
-                systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.d(TAG, "onCreate")
+
+        // Initialize fragments if this is the first creation
+        if (savedInstanceState == null) {
+            setupFragments()
+        }
     }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
+
+    private fun setupFragments() {
+        // Add ListFragment to the container
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_list, ListFragment.newInstance("param1", "param2"))
+            .commit()
+
+        // Add DetailFragment to the container
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_detail, DetailFragment.newInstance("param1", "param2"))
+            .commit()
     }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    companion object {
-        private const val TAG = "MainActivity"
+
+    override fun onSelected(id: Int) {
+        val detailFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_detail) as? DetailFragment
+        detailFragment?.setCoffeeData(id)
     }
 }
